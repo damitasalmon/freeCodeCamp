0607db0e56228ca93b8566e65c44893697217d06
Damita Salmon <code@iruldanet.com>
2023-07-03 17:38:05 -0400

    add old scipts previously saved to gists

---
+++ smallestCommonMultiple.js
@@ -0,0 +1,17 @@
+function smallestCommons(arr) {
+	arr.sort((a,b)=>a>b?1:-1)
+  let [div, num] = arr
+
+  while(div < arr[1]){
+      if(num % div===0) {
+				div++;
+				} else {
+       num += arr[1];
+			 div = arr[0]
+    }
+    }
+  return num;
+}
+
+console.log(smallestCommons([1,13]));
+//console.log(smallestCommons([23,18]));
